e69c158ed23ccadec55300e4ca56d9437e6a1d02
thiago-santanna <uzmaki_thiago@outlook.com>
2024-12-06 15:51:35 -0300

    tentando novo processar2

---
+++ processarpedido/src/main/java/com/tsswebapps/processarpedido/ProcessarpedidoApplication.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.processarpedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessarpedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessarpedidoApplication.class, args);
+	}
+
+}
---
+++ processarpedido/src/test/java/com/tsswebapps/processarpedido/ProcessarpedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.processarpedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessarpedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
